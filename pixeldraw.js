e363feb8733da15b5c7b6f4a207549aeb79b03fc
letsboogey <whaetbuoy9900@gmail.com>
2014-08-10 18:36:52 +0100

    40% progress

--- pixeldraw.js
+++ pixeldraw.js
@@ -1,6 +1,37 @@
 $(document).ready(function(){
 
+	//declare variables
+
+	//create accordion menu for sketch-pad options
 	$('#menu').accordion();
 
+	//slide footer out of view when mouse is not at bottom
+	$('#footer').hide();
+
+
+
+	//function to generate grid
+	function generateGrid(dimension){
+		var cols = dimension;
+		var rows = dimension;
+		
+		while(rows>0){
+		
+			var new_row = $('<div />',{'class':'row'});		
+			//divide the new row into sections = (dimension*dimension)
+			for(var k =0 ; k < cols ; k++){
+				$('<div />',{'class':'cell'}).appendTo(new_row);						
+			};
+
+			new_row.appendTo('.grid');
+			rows--;
+		};
+	};
+
+	generateGrid(50);
+
 	
+
+
+
 });
